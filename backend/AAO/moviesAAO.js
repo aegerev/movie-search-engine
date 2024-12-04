224ffc2fed170a1470fa8ae40041f14403bc2fee
alex <alexandersegerev@gmail.com>
2024-12-04 08:13:08 -0500

    Added code to access movies

---
+++ backend/AAO/moviesAAO.js
@@ -0,0 +1,17 @@
+let movies;
+
+export default class MoviesDAO{
+    static async injectDB(conn){
+        if(movies){
+            return;
+        }
+
+        try {
+            movies = await conn.db(process.env.MOVIEREVIEWS_NS).collection('movies');
+        } catch(e) {
+            console.error(`Unable to connect in MoviesAAO: ${e}`)
+        }
+    }
+
+    
+}
