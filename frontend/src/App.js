80029cb394046f196fef3c4151de14b50d169321
alex <alexandersegerev@gmail.com>
2024-12-20 10:42:12 -0500

    Fixed all errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import {Switch, Route, Link} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import MoviesList from './components/movies-list';
 import Movie from './components/movie';
-import Login from './components/login';
 
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar';
@@ -13,36 +12,30 @@ import './App.css';
 
 function App() {
 
-  let [user, setUser] = React.useState(null);
+  const user = React.useState(null);
 
-  async function login(user = null) {
-    setUser(user)
-  }
-
-  async function logout(user = null) {
-    setUser(user)
-  }
 
   return (
+    <BrowserRouter>
     <div className="App">
       <Navbar bg="light" expand="lg">
         <Navbar.Brand href="home">Movie Search Engine</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
       <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-          <Nav.Link href="/movies">Movies</Nav.Link>  
-          <Nav.Link>
-              {user ? (
-                <a onClick={logout}>Logout User</a>
-              ) : (
-                <a href="/login">Login</a>
-              )}
-            </Nav.Link>
+          <Nav.Link href="/">Movies</Nav.Link>  
           </Nav>
           </Navbar.Collapse>
       </Navbar>
+
+      <Routes>
+        <Route path="/movies" element={<MoviesList />} />
+        <Route path="/movies/:id" element={<Movie user={user} />} />
+      </Routes>
+
       <h1>Hi Ariel!</h1>
     </div>
+    </BrowserRouter>
   );
 }
 
