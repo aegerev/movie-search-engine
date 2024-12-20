3117e8f18c304c3e918cd35c49da36704ef9bdab
alex <alexandersegerev@gmail.com>
2024-12-20 08:54:34 -0500

    Added the Navbar and Nav imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,25 @@ import MoviesList from './components/movies-list';
 import Movie from './components/movie';
 import Login from './components/login';
 
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Ariel!</h1>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="home">Movie Search Engine</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>  
+            <Nav.Link href="#link">Link</Nav.Link>  
+          </Nav>
+          </Navbar.Collapse>
+      </Navbar>
+      <h1>Hi Ariel!</h1>
     </div>
   );
 }
